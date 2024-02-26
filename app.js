70ddab437a15c86d14a8353a7cb361c2278c0233
Abdul Rehman <154475349+AbdulRehman1632@users.noreply.github.com>
2024-02-26 13:03:29 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+
+// Q1:Write a script to greet your website visitor using JS alert
+// box.
+
+// alert("hello world")
+
+
+
+// Q2. Write a script to display following message on your web
+// page.
+
+// alert("Sorry!this link is not working.")
+
+
+
+// Q3. Write a script to display following message on your web
+// page: (Hint : Use line break)
+
+// alert("Welcome to Saylani \n Happy Coding!")
+
+
+
+// Q4. Write a script to display following messages in sequence
+
+// alert("welcome to saylani")
+
+// alert("Hello World")
+
+
+
+// Q5. Generate the following message through browser’s
+// developer console
+
+// console.log("hello world")
+
+
+
+
