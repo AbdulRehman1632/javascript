a4035e0a18af24edd28d7389d3f2e8033b7b2691
Abdul Rehman <154475349+AbdulRehman1632@users.noreply.github.com>
2024-03-10 14:02:58 -0700

    Add files via upload

---
+++ Chapter  4/app.js
@@ -0,0 +1,29 @@
+// chapter 4
+
+// 1. Declare 3 variables in one statement.
+
+// let name1 ,name2 ,name3 ;
+
+// 2. Declare 5 legal & 5 illegal variable names.
+
+// a) legal variable 
+
+// let name , _name , $name , name1 , _name1;
+
+// b) illegal variable
+
+// let 1name , -name , @name , 2_name , 2name5
+
+// 3.“Rules for naming JS variables”
+
+// Variable names can only contain
+// a. number b.underscore c.$ 
+
+// Variables must begin with a 
+// a. letter, b. $,  c. _ ,d.   
+
+
+//4. Variable names are case "sensitive"
+
+
+//5. Variable names should not be JS "KEYWORD"
