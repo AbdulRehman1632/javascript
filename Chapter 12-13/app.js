5c581a3ab383c67a1cb9a1e22700e70d7732c338
Abdul Rehman <154475349+AbdulRehman1632@users.noreply.github.com>
2024-04-07 05:58:48 -0700

    Add files via upload

---
+++ Chapter 12-13/app.js
@@ -0,0 +1,13 @@
+// 1. Write a program that takes a character (number or string)
+// in a variable & checks whether the given input is a
+// number, uppercase letter or lower case letter. (Hint: ASCII
+// codes:- A=65, Z=90, a=97, z=122).
+
+// ==========================================
+
+
+
+// 2. Write a JavaScript program that accept two integers and
+// display the larger. Also show if the two integers are equal.
+
+// ==========================================
